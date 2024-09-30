e00400c418d25b80ffb53bf89b782e207f8153ad
Suman Paudel <dastonsuman1997@gmail.com>
2024-09-30 21:46:29 +0545

    add configs and  gitignore

--- Anish Thapaliya/Hatespeech/config.py
+++ Anish Thapaliya/Hatespeech/config.py
@@ -1,20 +1,21 @@
 from enums import metric_enums, loss_enums
 from transformers import AutoTokenizer
 
-SEED = 2
+SEED = 22
 DEVICE = "cuda"
 MAX_LEN = 300
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 8
 EPOCHS = 10
 LEARNING_RATE=2e-5
-NUM_LABELS = 3
-# NUM_LABELS = 2
-HF_MODEL_PATH = "google/muril-large-cased"
-# TRAINING_FILE = "data/taskc/train.csv"
-# VALID_FILE = "data/taskc/valid.csv"
-TRAINING_FILE = "data/taskc/train.csv"
-VALID_FILE = "data/taskc/valid.csv"
+NUM_LABELS = 2
+# NUM_LABELS = 5
+HF_MODEL_PATH = "google-bert/bert-base-multilingual-uncased"
+# HF_MODEL_PATH = "rahular/varta-bert"
+# TRAINING_FILE = "data/taska/train.csv"
+# VALID_FILE = "data/taska/valid.csv"
+TRAINING_FILE = "data/taskb/train.csv"
+VALID_FILE = "data/taskb/valid.csv"
 TOKENIZER = AutoTokenizer.from_pretrained(HF_MODEL_PATH)
 MODEL_PATH = f"./models/{HF_MODEL_PATH.split('/')[-1]}"
 METRIC_NAME = metric_enums.MetricsEnum.F1_SCORE.value
