a49d0b57ea4129c5eb21d378ce151e99845eb326
Prabhat-123 <srv.ale52@gmail.com>
2024-09-30 21:57:10 +0545

    adding train valid synthetic combination dataset

--- Prabhat Ale/openai_service/merge_train_valid.py
+++ Prabhat Ale/openai_service/merge_train_valid.py
@@ -18,11 +18,29 @@ train_df = pd.read_csv(raw_train_csv_path)
 valid_df = pd.read_csv(raw_valid_csv_path)
 test_df = pd.read_csv(raw_test_csv_path)
 
-print(train_df.shape)
+
+
+
+
+
+
+synthetic_tweet_df = pd.concat([nepali_tweet_df, hindi_tweet_df])
+# Create the index
+index = np.arange(70001, 70001 + synthetic_tweet_df.shape[0])
+# If you want to assign this index to the DataFrame
+synthetic_tweet_df['index'] = index
+synthetic_tweet_df['label'] = 2
+final_synthetic_tweet_df = pd.DataFrame({'index': synthetic_tweet_df['index'],
+                                         'tweet': synthetic_tweet_df['Synthetic_Tweet'],
+                                         'label': synthetic_tweet_df['label']
+                                         })
+                                         
+# Drop columns with any NaN values
+final_synthetic_tweet_df = final_synthetic_tweet_df.dropna(how='any', axis=0)
+
+train_df = pd.concat([train_df, final_synthetic_tweet_df], axis = 0)
+
 
 train_valid_combined_df = pd.concat([train_df, valid_df], axis = 0)
-print(train_valid_combined_df.shape)
 
-
-
-train_df.to_csv(os.path.join(base_path, 'data/raw_train_valid_combination.csv'), index = False)
+train_valid_combined_df.to_csv(os.path.join(base_path, 'data/train_valid_synthetic_combination.csv'), index = False)
