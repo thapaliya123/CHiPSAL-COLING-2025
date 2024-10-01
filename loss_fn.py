381a73144d5a8e1e37a2bd3ec4d1b8a7c085b6d9
Suman Paudel <dastonsuman1997@gmail.com>
2024-10-01 18:43:36 +0545

    add loss function code

---
+++ loss_fn.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from enums.loss_enums import LossFuncEnum
+
+
+class LossFunction:
+    
+    weight = [2.0615, 2.5864, 7.7958]
+    def get_loss_func(loss_func: str):
+        
+        if loss_func == LossFuncEnum.BINARY_CROSSENTROPY.value:
+            return nn.BCELoss()
+    
+        if loss_func == LossFuncEnum.CATEGORICAL_CROSSENTROPY.value:
+            return nn.CrossEntropyLoss()
+        
+        if loss_func == LossFuncEnum.WEIGHTED_CROSSENTROPY.value:
+            return nn.CrossEntropyLoss(weight=LossFunction.weight)
+        
+        if loss_func == LossFuncEnum.FOCAL_CROSSENTROPY.value:
+            return FocalLoss()
+        
+
+class FocalLoss(nn.Module):
+    
+    def __init__(self, gamma=2.0, alpha=None, reduction=None):
+        super(FocalLoss, self).__init__()
+        self.gamma =gamma
+        self.alpha =alpha
+        self.reduction =reduction
+        
+    
+    def get_aggregated_loss(self, loss):
+        if self.reduction == 'mean':
+            return torch.mean(loss)
+        elif self.reduction == 'sum':
+            return torch.sum(loss)
+        elif self.reduction == 'none':
+            return loss
+        
+    def forward(self, inputs, targets):
+        
+        device = targets.device
+        ce_loss = F.cross_entropy(inputs, targets, reduction='none', weight=torch.tensor(self.alpha, device=device))
+
+        probs = torch.exp(-ce_loss)
+        focal_loss = ((1 - probs) ** self.gamma) * ce_loss
+
+        return self.get_aggregated_loss(focal_loss)
+        
+        
