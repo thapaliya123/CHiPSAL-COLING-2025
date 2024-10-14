c29c725fdcc9c75fb7aadaff3999aaa8ca7ceab3
thapaliya123 <thapaliyanish123@gmail.com>
2024-10-14 13:32:06 +0545

    add: exception handling

--- engine.py
+++ engine.py
@@ -24,33 +24,37 @@ def train_fn(data_loader, model, optimizer, device, scheduler, loss_fn_weights=N
     total_train_loss = 0
     print("### Training Started ###")
     for bi, d in tqdm(enumerate(data_loader), total=len(data_loader)):
-        ids = d["ids"]
-        token_type_ids = d["token_type_ids"]
-        mask = d["mask"]
-        targets = d["targets"]
-        # print("Training!!")
-        # print(f"ids: {ids}")
-        # print(f"token_type_ids: {token_type_ids}")
-        # print(f"mask: {mask}")
-        # print(f"targets: {targets}")
+        try:
+            ids = d["ids"]
+            token_type_ids = d["token_type_ids"]
+            mask = d["mask"]
+            targets = d["targets"]
+            # print("Training!!")
+            # print(f"ids: {ids}")
+            # print(f"token_type_ids: {token_type_ids}")
+            # print(f"mask: {mask}")
+            # print(f"targets: {targets}")
 
-        ids = torch.squeeze(ids).to(device, dtype=torch.long)
-        token_type_ids = torch.squeeze(token_type_ids).to(device, dtype=torch.long)
-        mask = torch.squeeze(mask).to(device, dtype=torch.long)
-        targets = targets.to(device, dtype=torch.long)
+            ids = torch.squeeze(ids).to(device, dtype=torch.long)
+            token_type_ids = torch.squeeze(token_type_ids).to(device, dtype=torch.long)
+            mask = torch.squeeze(mask).to(device, dtype=torch.long)
+            targets = targets.to(device, dtype=torch.long)
 
-        optimizer.zero_grad()
+            optimizer.zero_grad()
 
-        outputs = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
+            outputs = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
 
-        loss = loss_fn(outputs, targets, loss_fn_weights)
-        
-        total_train_loss += loss.item()
-        
-        loss.backward()
-        optimizer.step()
-        scheduler.step()
-    
+            loss = loss_fn(outputs, targets, loss_fn_weights)
+            
+            total_train_loss += loss.item()
+            
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+        except:
+            print("### Exception Ocurred in Valid Data Loader")
+
     avg_train_loss = total_train_loss/len(data_loader)
     return avg_train_loss
 
