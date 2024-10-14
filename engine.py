d290b3de036a9ead4dbde00cf84306c9a6d9534f
thapaliya123 <thapaliyanish123@gmail.com>
2024-10-14 13:04:25 +0545

    bug fix: shape error when using full models

--- engine.py
+++ engine.py
@@ -63,25 +63,28 @@ def eval_fn(data_loader, model, device):
     print("### Evaluation Started ###")
     with torch.no_grad():
         for bi, d in tqdm(enumerate(data_loader), total=len(data_loader)):
-            ids = d["ids"]
-            token_type_ids = d["token_type_ids"]
-            mask = d["mask"]
-            targets = d["targets"]
+            try:
+                ids = d["ids"]
+                token_type_ids = d["token_type_ids"]
+                mask = d["mask"]
+                targets = d["targets"]
 
-            ids = torch.squeeze(ids).to(device, dtype=torch.long)
-            token_type_ids = torch.squeeze(token_type_ids).to(device, dtype=torch.long)
-            mask = torch.squeeze(mask).to(device, dtype=torch.long)
-            targets = targets.to(device, dtype=torch.long)
+                ids = torch.squeeze(ids).to(device, dtype=torch.long)
+                token_type_ids = torch.squeeze(token_type_ids).to(device, dtype=torch.long)
+                mask = torch.squeeze(mask).to(device, dtype=torch.long)
+                targets = targets.to(device, dtype=torch.long)
 
 
-            outputs = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
-            loss = loss_fn(outputs, targets)
-            
-            total_valid_loss += loss.item()
-            
-            fin_targets.extend(targets.cpu().detach().numpy().tolist())
-            m = nn.Softmax(dim=1)
-            fin_outputs.extend(m(outputs).cpu().detach().numpy().tolist())
+                outputs = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
+                loss = loss_fn(outputs, targets)
+                
+                total_valid_loss += loss.item()
+                
+                fin_targets.extend(targets.cpu().detach().numpy().tolist())
+                m = nn.Softmax(dim=1)
+                fin_outputs.extend(m(outputs).cpu().detach().numpy().tolist())
+            except:
+                print("Exception Ocurred in Valid Data Loader")
     avg_valid_loss = total_valid_loss / len(data_loader)
     return fin_outputs, fin_targets, avg_valid_loss
 
